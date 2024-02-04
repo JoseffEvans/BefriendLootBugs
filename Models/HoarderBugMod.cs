8acb09942f8c078f75df721d35b8268ebc79acad
Joseff Evans <joseffevans@outlook.com>
2024-02-04 17:37:22 +0000

    Allow them outside

--- Models/HoarderBugMod.cs
+++ Models/HoarderBugMod.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using GameNetcodeStuff;
+using UnityEngine;
 
 namespace BefriendLootBugs{
     public class HoarderBugMod(HoarderBugAI hoarderBug){
@@ -8,6 +9,8 @@ namespace BefriendLootBugs{
         public HoarderBugAI HoarderBug {get;} = hoarderBug;
         public bool IsFriendly {get; set;} = false;
         public PlayerControllerB Friend {get; set;}
+
+        public bool FriendOutside => Friend.thisPlayerBody.transform.position.y > -80f;
 
         public static HoarderBugMod Find(HoarderBugAI hoarderBug){
             if(!HoarderBugMods.ContainsKey(hoarderBug))
@@ -28,6 +31,9 @@ namespace BefriendLootBugs{
             HoarderBug.movingTowardsTargetPlayer = true;
             HoarderBug.isAngry = false;
             HoarderBug.annoyanceMeter = 0f;
+
+            if(FriendOutside && !HoarderBug.isOutside)
+                TeleportOutside();
         }
 
 
@@ -40,6 +46,43 @@ namespace BefriendLootBugs{
         public void Grab(GrabbableMod grabbable){
             if(grabbable.IsGifted)
                 Befriend(grabbable.LastOwner);
+        }
+
+        
+        public void TeleportOutside()
+        {
+            Vector3 doorPostition = RoundManager.Instance.GetNavMeshPosition(
+                RoundManager.FindMainEntrancePosition(getTeleportPosition: true, true));
+
+            Teleport(doorPostition);
+
+            HoarderBug.isOutside = true;
+            HoarderBug.allAINodes = GameObject.FindGameObjectsWithTag("OutsideAINode");
+
+            PlayDoorAudio();
+        }
+
+        void Teleport(Vector3 position){
+            if (HoarderBug.IsOwner)
+            {
+                HoarderBug.agent.enabled = false;
+                HoarderBug.transform.position = position;
+                HoarderBug.enabled = true;
+            }
+            else
+            {
+                HoarderBug.transform.position = position;
+            }
+            HoarderBug.serverPosition = position;
+        }
+
+        void PlayDoorAudio(){
+            EntranceTeleport entranceTeleport = RoundManager.FindMainEntranceScript(true);
+            if (entranceTeleport.doorAudios != null && entranceTeleport.doorAudios.Length != 0)
+            {
+                entranceTeleport.entrancePointAudio.PlayOneShot(entranceTeleport.doorAudios[0]);
+                WalkieTalkie.TransmitOneShotAudio(entranceTeleport.entrancePointAudio, entranceTeleport.doorAudios[0]);
+            }
         }
     }
 }
